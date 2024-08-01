f577fb7eaf3b869d3b769bcf937e9ffddf4f5c86
Raylight-Developer <Raylight.creator@gmail.com>
2024-08-01 17:43:18 -0600

    Commit

--- Lab 02/Parte 2/emisor.py
+++ Lab 02/Parte 2/emisor.py
@@ -1,11 +1,11 @@
 import socket
 import struct
 import matplotlib.pyplot as plt
+from matplotlib.ticker import FuncFormatter
 from sympy import sympify
 
 from crc32 import *
 from hamming import *
-
 WORD_MIN_LENGTH = 2
 WORD_MAX_LENGTH = 4
 NOISE_FACTOR_START = 0
@@ -14,11 +14,34 @@ NOISE_FACTOR_INC   = 0.1
 ERROR_RATE_START = 0
 ERROR_RATE_END   = 7
 ERROR_RATE_INC   = 0.005
-COUNT = 8
+COUNT = 256
+LOG_STATS = True
+PRINT = False
+LOG = False
+GRAPH = True
+FILE = "Stats.html"
+"""
+WORD_MIN_LENGTH = 2
+WORD_MAX_LENGTH = 15
+NOISE_FACTOR_START = 1
+NOISE_FACTOR_END   = 1
+NOISE_FACTOR_INC   = 0.25
+ERROR_RATE_START = 1
+ERROR_RATE_END   = 1
+ERROR_RATE_INC   = 0.005
+COUNT = 1024
+LOG_STATS = True
 PRINT = False
 LOG = True
+GRAPH = False
+FILE = "Log.html"
+"""
 
 noise_variance: List[Tuple[float, List[Tuple[float, int]]]] = []
+
+def strp(number_str: str):
+	if '.' in number_str: return number_str.rstrip('0').rstrip('.')
+	return number_str
 
 def select_random_words(filename):
 	return ' '.join(random.sample(open(filename, 'r').read().split(), random.randrange(WORD_MIN_LENGTH, WORD_MAX_LENGTH)))
@@ -29,7 +52,7 @@ def main():
 
 	host='127.0.0.1'
 	port=8888
-	if LOG: open('Log.html', 'w').write("")
+	if LOG: open(FILE, 'w').write("")
 	with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as client_socket:
 		client_socket.connect((host, port))
 
@@ -44,11 +67,10 @@ def main():
 					data = encode_hamming(str_to_bits(message))
 					crc = crc32_encode(str_to_bits(message))
 					noisy_data = hamming_noise(data, sympify(noise_rate * NOISE_FACTOR_INC), sympify(error_rate * ERROR_RATE_INC))
-					#if i == MULTI_ERROR_FACTOR_START:
 					if LOG :
-						open('Log.html', 'a').write(f"\n<pre>\n[Send] Message: {message}")
-						open('Log.html', 'a').write(f"\n[Send] Hamming: {list_str(data)}")
-						open('Log.html', 'a').write(f"\n[Send] Noisy  : {list_str(noisy_data)}")
+						open(FILE, 'a').write(f"\n<pre>\n[Send] Message: {message}")
+						open(FILE, 'a').write(f"\n[Send] Hamming: {list_str(data)}")
+						open(FILE, 'a').write(f"\n[Send] Noisy  : {list_str(noisy_data)}")
 
 					client_socket.sendall(list_str(noisy_data).encode() + struct.pack('!I', crc))
 
@@ -57,39 +79,35 @@ def main():
 						data = [int(bit) for bit in str(response[:-4])[2:-1]]
 						crc_rec = struct.unpack('!I', response[-4:])[0]
 						ham, err = decode_hamming(data, PRINT)
-						#if i == MULTI_ERROR_FACTOR_START:
 						if LOG:
-							open('Log.html', 'a').write(f"\n<y>[Hamming]</y> Errors: {err}")
-							open('Log.html', 'a').write(f"\n<g>[Hamming]</g> Decoded: {list_str(ham)}")
+							open(FILE, 'a').write(f"\n<y>[Hamming]</y> Errors: {err}")
+							open(FILE, 'a').write(f"\n<g>[Hamming]</g> Decoded: {list_str(ham)}")
 						if PRINT: print(f"\033[32m[Hamming]\033[0m Decoded: {list_str(ham)}")
 						crc_calc = crc32_encode(ham)
 						if crc_rec != crc_calc:
-							#if i == MULTI_ERROR_FACTOR_START:
 							if PRINT: print(f"\033[31m[CRC32]\033[0m Failed {crc_rec:#10x} != {crc_calc:#10x}")
-							if LOG: open('Log.html', 'a').write(f"\n<r>[CRC32]</r> Failed {crc_rec:#10x} != {crc_calc:#10x}")
+							if LOG: open(FILE, 'a').write(f"\n<r>[CRC32]</r> Failed {crc_rec:#10x} != {crc_calc:#10x}")
 							errors += 1
 						else:
-							#if i == MULTI_ERROR_FACTOR_START:
 							if PRINT:
 								print(f"\033[32m[CRC32]\033[0m Verified {crc_rec:#10x} == {crc_calc:#10x}")
 								print(f"\033[32m[Rec]\033[0m : {bits_to_str(ham)}")
 							if LOG:
-								open('Log.html', 'a').write(f"\n<g>[CRC32]</g> Verified {crc_rec:#10x} == {crc_calc:#10x}")
-								open('Log.html', 'a').write(f"\n<g>[Rec]</g> : {bits_to_str(ham)}")
-									
-						#if i == MULTI_ERROR_FACTOR_START:
+								open(FILE, 'a').write(f"\n<g>[CRC32]</g> Verified {crc_rec:#10x} == {crc_calc:#10x}")
+								open(FILE, 'a').write(f"\n<g>[Rec]</g> : {bits_to_str(ham)}")
 						if PRINT: print("|------------------------------------------------------------------------------------")
-						if LOG: open('Log.html', 'a').write("\n</pre>")
-				print(f"Error Factor: {str(sympify(error_rate* ERROR_RATE_INC*100)).rstrip('0')}%")
-				error_variance.append((str(sympify(error_rate* ERROR_RATE_INC*100)).rstrip('0'), errors))
+						if LOG: open(FILE, 'a').write("\n</pre>")
+				print(f"Error Factor: {strp(str(sympify(error_rate* ERROR_RATE_INC*100)))} %")
+				error_variance.append((strp(str(sympify(error_rate* ERROR_RATE_INC*100))), errors))
 				error_rate += 1
-			print(f"Noise Factor: {str(sympify(noise_rate* NOISE_FACTOR_INC*100)).rstrip('0')}%")
-			noise_variance.append((str(sympify(noise_rate* NOISE_FACTOR_INC*100)).rstrip('0'), error_variance))
+			print(f"Noise Factor: {strp(str(sympify(noise_rate* NOISE_FACTOR_INC*100)))} %")
+			noise_variance.append((strp(str(sympify(noise_rate* NOISE_FACTOR_INC*100))), error_variance))
 			noise_rate += 1
 	if LOG:
-		open('Log.html', 'a').write("\n<body>")
-		existing_content = open('Log.html', 'r').read()
-		open('Log.html', 'w').write(
+		open(FILE, 'a').write("\n<body>")
+		existing_content = open(FILE, 'r').read()
+	if LOG_STATS or LOG:
+		open(FILE, 'w').write(
 f"""<body>
 <style>
 * {{
@@ -133,32 +151,42 @@ y {{
 }}
 </style>
 """)
+		if GRAPH:
+			open(FILE, 'a').write(f"""
+		<img src = "Stress.png"></img>
+		""")
 
-	for noise_rate, data in noise_variance:
-		for error_rate, error in data:
-			open('Log.html', 'a').write(f"""
+		for noise_rate, data in noise_variance:
+			for error_rate, error in data:
+				open(FILE, 'a').write(f"""
 <pre>
-Noise Rate: {noise_rate}%
-Flip Rate : {error_rate}%
+Noise Rate: {noise_rate} %
+Flip Rate : {error_rate} %
 MESSAGE   Count: {COUNT}
 <g>SUCCESSES</g> Count: {COUNT - error} {((COUNT-error) / (COUNT))*100}%
 <r>FAILURES</r>  Count: {error} {((error) / (COUNT))*100}%
 </pre>
 """ )
-	open('Log.html', 'a').write(existing_content)
+	if LOG:
+		open(FILE, 'a').write(existing_content)
 
 main()
 
-plt.figure(figsize=(10, 6))
+if GRAPH:
+	plt.figure(figsize=(10, 6))
 
-for (x_value, points) in noise_variance:
-	x = [point[0] for point in points]
-	y = [point[1] for point in points]
-	plt.plot(x, y, marker='o', label=f"Noise {x_value}%")
+	for (noise_factor, error_list) in noise_variance:
+		x = [error_percent[0] for error_percent in error_list]
+		y = [sympify((error_count[1] / COUNT) * 100) for error_count in error_list]
+		plt.plot(x, y, marker='o', label=f"Noise {noise_factor} %")
 
-plt.xlabel('Bit Flip%')
-plt.ylabel('Error Rate')
-plt.title('Graph of Noise Impact')
-plt.legend()
-plt.grid(True)
-plt.savefig('Plot.png')
+	plt.xlabel('White Noise %')
+	plt.ylabel('Hamming Noise %')
+	plt.title('Graph of Noise Impact')
+	plt.legend()
+	plt.grid(True)
+	def percent_formatter(x, pos):
+		return f'{x} %'
+	plt.gca().xaxis.set_major_formatter(FuncFormatter(percent_formatter))
+	plt.gca().yaxis.set_major_formatter(FuncFormatter(percent_formatter))
+	plt.savefig('Stress.png')
